af3b05e44391536f2ed480a2f2c48f5e7523afa6
MikiKru <michal_kruczkowski@o2.pl>
2020-11-07 13:41:23 +0100

    spring v11

--- src/main/java/pl/sda/spring_start/service/UserService.java
+++ src/main/java/pl/sda/spring_start/service/UserService.java
@@ -5,13 +5,23 @@ import org.springframework.stereotype.Service;
 import pl.sda.spring_start.model.User;
 import pl.sda.spring_start.repository.UserRepository;
 
+import java.util.Optional;
+
 @Service            // servis - implementuje logikę aplikacji
 public class UserService {
     @Autowired      // wstrzykiwanie zależności
     UserRepository userRepository;
 
     public void registerUser(User user){
-        userRepository.save(user);          // INSERT INTO user valsues (?,?,?,?)
+        userRepository.save(user);          // INSERT INTO user values (?,?,?,?)
+    }
+    public void activateUser(int userId){   // UPDATE user SET status = 1 WHERE user_id = ?;
+        Optional<User> userOptional = userRepository.findById(userId);
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            user.setStatus(true);
+            userRepository.save(user);      // save gdy jest wywoływana na istniejącym w db obiekcie to działa jak update
+        }
     }
 
 }
