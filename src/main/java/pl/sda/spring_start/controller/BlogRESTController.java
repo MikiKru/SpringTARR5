a3e36a7092a804fac56a02adf015accaf4b75a83
MikiKru <michal_kruczkowski@o2.pl>
2020-11-07 10:53:27 +0100

    spring v3

--- src/main/java/pl/sda/spring_start/controller/BlogRESTController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogRESTController.java
@@ -2,6 +2,7 @@ package pl.sda.spring_start.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // klasa mapująca żądania prokołu http - adres lokalny http://localhost:8080
@@ -12,7 +13,7 @@ public class BlogRESTController {
     // POST     - INSERT - wprowadza dane do bazy i nic nie zwraca
     // PUT      - UPDATE - edytuje dane z bazy i nic nie zwraca
     // DELETE   - DELETE - usowanie obiektu lub listy obiektów z bazy
-    @GetMapping("/")                // localhost:8080/
+    @GetMapping("/")                         // localhost:8080/
     public String home(){
         return "Hello in homepage";
     }
@@ -21,7 +22,15 @@ public class BlogRESTController {
             @PathVariable("name") String name,
             @PathVariable("status") Boolean status
             ){
-        return status ? "Hello " + name + " in hompage" : "Twoje konto jest nieaktywne";
+        return status ? "Hello " + name + " in hompage" : "Your account is locked";
     }
+    @GetMapping("/credentials")
+    public String getCredentials(
+            @RequestParam("login") String login,
+            @RequestParam("password") String password
+    ){
+        return String.format("login : %s \npassword : %s", login, password);
+    }
+
 
 }
