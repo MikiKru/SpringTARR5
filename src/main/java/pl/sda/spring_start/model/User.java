e35c74de505483b2eb89d46c0e87b8f97631c4b9
MikiKru <michal_kruczkowski@o2.pl>
2020-11-07 12:17:19 +0100

    spring v7

--- src/main/java/pl/sda/spring_start/model/User.java
+++ src/main/java/pl/sda/spring_start/model/User.java
@@ -1,6 +1,23 @@
 package pl.sda.spring_start.model;
 // klasa determinująca strukturę danych w projekcie
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity         // determinuje mapowanie klasy na tabelkę DB
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    private int userId;             // auto inkrementowany klucz głównym tabeli
+    private String email;
+    private String password;
+    private LocalDateTime registrationDateTime;
+    private boolean status;
 
 }
