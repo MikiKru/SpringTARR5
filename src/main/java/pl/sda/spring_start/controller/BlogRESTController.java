b8373ca6de7a3b6091e1ac968c679cf89d400664
MikiKru <michal_kruczkowski@o2.pl>
2020-11-07 10:47:15 +0100

    spring v2

---
+++ src/main/java/pl/sda/spring_start/controller/BlogRESTController.java
@@ -0,0 +1,27 @@
+package pl.sda.spring_start.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// klasa mapująca żądania prokołu http - adres lokalny http://localhost:8080
+//@Controller       //- mapuje żądanie i zwraca widok html
+@RestController     //- mapuje żądania i dane REST - Reprentative State Transfer
+public class BlogRESTController {
+    // GET      - SELECT - pobiera zawartość z bazy i zwraca obiekt lub listę obiektów
+    // POST     - INSERT - wprowadza dane do bazy i nic nie zwraca
+    // PUT      - UPDATE - edytuje dane z bazy i nic nie zwraca
+    // DELETE   - DELETE - usowanie obiektu lub listy obiektów z bazy
+    @GetMapping("/")                // localhost:8080/
+    public String home(){
+        return "Hello in homepage";
+    }
+    @GetMapping("/home/{name}&{status}")     // localhost:8080/home/Michal&true
+    public String homeWithName(
+            @PathVariable("name") String name,
+            @PathVariable("status") Boolean status
+            ){
+        return status ? "Hello " + name + " in hompage" : "Twoje konto jest nieaktywne";
+    }
+
+}
