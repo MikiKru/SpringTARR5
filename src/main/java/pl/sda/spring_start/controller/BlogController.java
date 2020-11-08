06a9359e41756a7abd6479fa7996fbb1de2273c0
MikiKru <michal_kruczkowski@o2.pl>
2020-11-08 14:14:31 +0100

    spring d2.16

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -1,17 +1,26 @@
 package pl.sda.spring_start.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.spring_start.service.PostService;
+import pl.sda.spring_start.service.UserService;
 
 @Controller     // klasa mapująca url na wywołanie metody i zwracająca widok html
 public class BlogController {
+    private UserService userService;
+    private PostService postService;
+    @Autowired
+    public BlogController(UserService userService, PostService postService) {
+        this.userService = userService;
+        this.postService = postService;
+    }
     @GetMapping("/")        // na adresie localhost:8080/
     public String home(Model model){   // wywołaj metodę home()
         // dodaje atrybut do obiektu model, który może być przekazany do widoku
         // model.addAttribute(nazwaAtrybutu, wartość);
-        model.addAttribute("title", "Michael's blog");
-        model.addAttribute("subtitle", "The best blog in the World");
+        model.addAttribute("posts", postService.getAllPosts());
         return "index";     // zwracającą nazwę dokumentu html który ma być wyświetlany
     }
 }
