65589e1ec694b31492ac468fb49c21674332a5a8
MikiKru <michal_kruczkowski@o2.pl>
2020-11-08 12:33:41 +0100

    spring d2.13

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -1,12 +1,17 @@
 package pl.sda.spring_start.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller     // klasa mapująca url na wywołanie metody i zwracająca widok html
 public class BlogController {
     @GetMapping("/")        // na adresie localhost:8080/
-    public String home(){   // wywołaj metodę home()
+    public String home(Model model){   // wywołaj metodę home()
+        // dodaje atrybut do obiektu model, który może być przekazany do widoku
+        // model.addAttribute(nazwaAtrybutu, wartość);
+        model.addAttribute("title", "Michael's blog");
+        model.addAttribute("subtitle", "The best blog in the World");
         return "index";     // zwracającą nazwę dokumentu html który ma być wyświetlany
     }
 }
