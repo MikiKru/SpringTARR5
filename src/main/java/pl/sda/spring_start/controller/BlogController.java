a961db4fb28143359f625aafc6d9682fec6d82f9
MikiKru <michal_kruczkowski@o2.pl>
2020-11-08 15:42:27 +0100

    spring d2.18

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import pl.sda.spring_start.model.Category;
 import pl.sda.spring_start.model.Post;
 import pl.sda.spring_start.service.PostService;
 import pl.sda.spring_start.service.UserService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Controller     // klasa mapująca url na wywołanie metody i zwracająca widok html
@@ -32,5 +35,11 @@ public class BlogController {
         Optional<Post> postOptional = postService.getPostById(postId);
         postOptional.ifPresent(post -> model.addAttribute("post", post));
         return "post";
+    }
+    @GetMapping("/addPost")                 // przejście metodą GET na stronę formularze
+    public String addPost(Model model){     // i przekazanie pustego obiektu Post
+        model.addAttribute("post", new Post());
+        model.addAttribute("categories", new ArrayList<>(Arrays.asList(Category.values())));
+        return "addPost";
     }
 }
