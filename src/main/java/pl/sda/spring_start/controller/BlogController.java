c7c6af68b40fcad04865e5e19a5fe695762d65ba
MikiKru <michal_kruczkowski@o2.pl>
2020-11-08 14:45:30 +0100

    spring d2.17

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.sda.spring_start.model.Post;
 import pl.sda.spring_start.service.PostService;
 import pl.sda.spring_start.service.UserService;
+
+import java.util.Optional;
 
 @Controller     // klasa mapująca url na wywołanie metody i zwracająca widok html
 public class BlogController {
@@ -22,5 +26,11 @@ public class BlogController {
         // model.addAttribute(nazwaAtrybutu, wartość);
         model.addAttribute("posts", postService.getAllPosts());
         return "index";     // zwracającą nazwę dokumentu html który ma być wyświetlany
+    }
+    @GetMapping("/posts&{postId}")
+    public String getPost(@PathVariable("postId") int postId, Model model){
+        Optional<Post> postOptional = postService.getPostById(postId);
+        postOptional.ifPresent(post -> model.addAttribute("post", post));
+        return "post";
     }
 }
