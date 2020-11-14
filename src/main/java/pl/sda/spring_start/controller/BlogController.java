3f900a96dfc2622b48c554311fa2cd979d467217
MikiKru <michal_kruczkowski@o2.pl>
2020-11-14 09:34:59 +0100

    spring d3.1

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.spring_start.model.Category;
 import pl.sda.spring_start.model.Post;
 import pl.sda.spring_start.service.PostService;
@@ -40,6 +42,17 @@ public class BlogController {
     public String addPost(Model model){     // i przekazanie pustego obiektu Post
         model.addAttribute("post", new Post());
         model.addAttribute("categories", new ArrayList<>(Arrays.asList(Category.values())));
-        return "addPost";
+        return "addPost";                   // tu znajduje się formularz i jest uzupłeniany przez użytkownika
+                                            // gdy wprowadza pola do formularza to set-uje pola klasy Post
     }
+    @PostMapping("/addPost")                // przekazanie parametrów z formularza metodą POST
+    public String addPost(
+            @ModelAttribute Post post       // obiekt model przekazuje obiekt post do metody
+    ){
+        // zapisanie nowego posta do db
+        postService.addPost(post.getTitle(), post.getContent(), post.getCategory(),
+                userService.getUserById(3).get());  // rozwiązanie na chwilę !!!
+        return "redirect:/";                // przekierowuje na ades, który zwraca jakiś widok
+    }
+
 }
